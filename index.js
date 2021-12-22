436e23f45a63665ea00880ccea0d1e7b5d8d4b90
ilyosbek <ilyosbek.inomjonov.1996@gmail.com>
2021-12-23 00:28:40 +0500

    Done

---
+++ index.js
@@ -0,0 +1,8 @@
+// data tekshirish
+
+function app(value1, value2, value3) {
+    var data = 'Shakhzod Shukrulloyev'
+    if (data.includes(value1 && value2 && value3)) console.log(true);
+    else console.log(false);
+}
+app ('Otabek', 'Elbek', 'Shakhzod')
